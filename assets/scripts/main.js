4fddb9256d054ca6a0b243cb314b6626f76db601
Adeleke Togun <adeleketogun@gmail.com>
2015-05-10 15:24:23 +0100

    Clean up code, get current screen width

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-  $('.scrollspy').scrollSpy();
-});
+'use strict'
+
+var dc = {
+  getBrowserWindow: function () {
+    var width = $(window).width();
+    window.onresize = function() {
+      width = $(window).width();
+      console.log(width);
+    };
+  },
+
+  init: function () {
+    dc.getBrowserWindow();
+  }
+};
+
+$(document).ready(dc.init);
+
