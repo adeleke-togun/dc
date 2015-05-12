521b0bd67c706a67b912859d505e31ce4dd76357
Adeleke Togun <adeleketogun@gmail.com>
2015-05-12 01:12:10 +0100

    Recreate Timer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -32,5 +32,15 @@ var dc = {
     dc.fullpage();
   }
 };
+// TIMER CODE BLOCK
+var deadLineDate = new Date(2015, 4, 27, 23, 59, 59); // May 27th, 23:59:59 PM
 
+var timerId = countdown(deadLineDate, function(ts) {
+  document.getElementById('clock-timer').innerHTML = ts.toHTML("span");
+}, countdown.DEFAULTS);
+
+// later on this timer may be  with the right condition
+if (false) {
+  window.clearInterval(timerId);
+}
 $(document).ready(dc.init);
